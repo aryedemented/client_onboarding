8f2e735047f0e86e248b193a999c503f2b41cbf8
AryeDemented <arye.lerner@gmail.com>
2025-05-09 18:36:44 +0300

    docker running locally

--- client_boarding/run_onboarding.py
+++ client_boarding/run_onboarding.py
@@ -1,9 +1,12 @@
 import sys
 import subprocess
-
+import os
 
 python_executable = sys.executable
 
-command = [python_executable, "-m", "streamlit", "run", "main.py"]
+# Make sure the path is correct relative to the Docker WORKDIR
+script_path = os.path.join(os.path.dirname(__file__), "main.py")
+
+command = [python_executable, "-m", "streamlit", "run", script_path]
 
 subprocess.run(command)
